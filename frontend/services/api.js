18c54e3b56760e065ca8e92916614cc652a9b226
EinWelt <achebejc@gmail.com>
2026-04-19 14:47:35 -0400

    Add API service with axios and token interceptor

---
+++ frontend/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+export const api = axios.create({
+  baseURL: API_URL,
+  timeout: 15000,
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
